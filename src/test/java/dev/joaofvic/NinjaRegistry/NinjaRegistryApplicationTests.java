6aa372ddd9197807a7f2f711315f97eb914ed68d
joaofvic <fjoao2012v@gmail.com>
2025-08-30 20:16:51 -0300

    first commit

---
+++ src/test/java/dev/joaofvic/NinjaRegistry/NinjaRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.joaofvic.NinjaRegistry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NinjaRegistryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
