1fe94611d3dd6b8b98871b4a17606446cea4223a
joaofvic <fjoao2012v@gmail.com>
2025-08-30 22:22:26 -0300

    Controller created [Class 2]

---
+++ src/main/java/dev/joaofvic/NinjaRegistry/Controller.java
@@ -0,0 +1,15 @@
+package dev.joaofvic.NinjaRegistry;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
+public class Controller {
+	
+	@GetMapping("/welcome")
+	public String welcome() {
+		return "This is my first message on this route";
+	}
+}
