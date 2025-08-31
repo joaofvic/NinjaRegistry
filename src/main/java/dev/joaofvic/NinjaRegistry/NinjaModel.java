de0d3fcd7187d18e5b9c1a7feb4b47af3b4d1dcd
joaofvic <fjoao2012v@gmail.com>
2025-08-31 00:33:07 -0300

    Create an entity in the database from the NinjaModel [Class 3]

---
+++ src/main/java/dev/joaofvic/NinjaRegistry/NinjaModel.java
@@ -0,0 +1,53 @@
+package dev.joaofvic.NinjaRegistry;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tb_register")
+public class NinjaModel {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String name;
+	private String email;
+	private int age;
+	
+	public NinjaModel() {
+	}
+
+	public NinjaModel(String name, String email, int age) {
+		this.name = name;
+		this.email = email;
+		this.age = age;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+	
+}
