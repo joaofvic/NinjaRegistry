9b5de7dab9ea5b525025b9714c9161c0d8b8ce8f
joaofvic <fjoao2012v@gmail.com>
2025-09-05 22:10:59 -0300

    feat: create controller routes with mock

--- src/main/java/dev/joaofvic/NinjaRegistry/Missions/MissionsController.java
+++ src/main/java/dev/joaofvic/NinjaRegistry/Missions/MissionsController.java
@@ -1,4 +1,29 @@
 package dev.joaofvic.NinjaRegistry.Missions;
 
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("missions")
 public class MissionsController {
+
+    @GetMapping("/all")
+    public String allMisions() {
+        return "All missions here";
+    }
+
+    @PostMapping("/create")
+    public String createMission() {
+        return "Mission created";
+    }
+
+    @PutMapping("/change")
+    public String changeMission() {
+        return "Mission change successful";
+    }
+
+    @DeleteMapping("/delete")
+    public String deleteMission() {
+        return "Mission deleted";
+    }
+
 }
