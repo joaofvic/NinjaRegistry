055897da6b622ec2a9ae1dd28879fb6b3bf6150a
joaofvic <fjoao2012v@gmail.com>
2025-09-06 20:06:56 -0300

    fix: changes the controller route to list the ninjas and injects the SERVICE and REPOSITORY dependency

--- src/main/java/dev/joaofvic/NinjaRegistry/Ninjas/NinjaController.java
+++ src/main/java/dev/joaofvic/NinjaRegistry/Ninjas/NinjaController.java
@@ -2,42 +2,51 @@ package dev.joaofvic.NinjaRegistry.Ninjas;
 
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
-@RequestMapping
+@RequestMapping("/ninja")
 public class NinjaController {
-	
-	@GetMapping("/welcome")
+
+    private NinjaService ninjaService;
+
+    public NinjaController(NinjaService ninjaService) {
+        this.ninjaService = ninjaService;
+    }
+
+    @GetMapping("/welcome")
 	public String welcome() {
 		return "This is my first message on this route";
 	}
 
     // Add ninja
-    @PostMapping("./register")
+    @PostMapping("/register")
     public String register() {
         return "Ninja created";
     }
 
     // Show ninjas
-    @GetMapping("./all")
-    public String all() {
-        return "Ninja showed";
+    @GetMapping("/all")
+    public List<NinjaModel> all() {
+        return ninjaService.listNinjas();
     }
 
     // Show ninjas by Id
-    @GetMapping("./allId")
+    @GetMapping("/allId")
     public String allId() {
         return "Ninja showed by Id";
     }
 
     // Change ninja data
-    @PutMapping("./changeId")
+    @PutMapping("/changeId")
     public String changeId() {
         return "Changed ninja by Id";
     }
 
     // Delete ninja
-    @DeleteMapping("./deleteId")
+    @DeleteMapping("/deleteId")
     public String deleteId() {
         return "Deleted ninja by Id";
     }
+
 }
