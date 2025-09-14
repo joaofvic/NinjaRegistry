8b56cf91f992cc877dc076a2489b5408ee4cf574
joaofvic <fjoao2012v@gmail.com>
2025-09-14 04:46:02 -0300

    chore: includes the DTO version for the entity and manually maps the entity x dto paths in NinjaMapper

--- src/main/java/dev/joaofvic/NinjaRegistry/Ninjas/NinjaDTO.java
+++ src/main/java/dev/joaofvic/NinjaRegistry/Ninjas/NinjaDTO.java
@@ -1,4 +1,20 @@
 package dev.joaofvic.NinjaRegistry.Ninjas;
 
+import dev.joaofvic.NinjaRegistry.Missions.MissionsModel;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class NinjaDTO {
+
+    private Long id;
+    private String name;
+    private String email;
+    private String imgUrl;
+    private int age;
+    private String rank;
+    private MissionsModel missions;
 }
